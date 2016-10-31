8e385ccf978a74df7693447e259115706641e1b6
Aetherunbound <bowdenm@spu.edu>
2016-10-30 18:37:52 -0700

    Spooky Detector now works as intended! Audio is tab and volume dependent based on spooks found in current tab, popup style improved.

--- background.js
+++ background.js
@@ -1,47 +1,110 @@
-var activeTab = 0;
-var spookCount = 0;
+window.onload = function() {
+	var activeTab = 0;
+	var spookCount = 0;
+	var stoppedTabs = {};
 
-function getSpooks(activeTab) {
-	chrome.tabs.sendMessage(activeTab, {detail: "count"}, function sendResponse(count) {
-		if(count != null) {
-			console.log("Number of spooks on page: " + count);
-			spookCount = count;
+	try{
+		var allElements = document.getElementsByTagName("*");
+		var sound = document.getElementById("spook_sound");
+	}
+	catch(err) {
+		console.log("issue with declaring");
+	}
+
+	function playSound() {
+		if(stoppedTabs[activeTab] === undefined)
+			sound.play();
+		else
+			pauseSound();
+	}
+
+	function pauseSound() {
+		sound.pause();
+	}
+
+	function stopSound() {
+		stoppedTabs[activeTab] = 1;
+		sound.pause();
+		sound.currentTime = 0;
+	}
+
+	function setVolume(count) {
+		volume = 0;
+		if (count < 25)
+			volume = count/25;
+		else 
+			volume = 1;
+		sound.volume = volume
+		console.log("Volume: " + volume);
+	}
+
+	function getSpooks(activeTab, play = true) {
+		chrome.tabs.sendMessage(activeTab, {detail: "count"}, function sendResponse(count) {
+			if(count != null) {
+				console.log("Number of spooks on page: " + count + ", " + play);
+				spookCount = count;
+			}	
+			if(play){
+				if (spookCount > 0) {
+					setVolume(spookCount);
+					playSound();
+				}
+				else
+					pauseSound();
+			}
+		});
+	}
+
+	//Listener for the connection call from the content script
+	chrome.runtime.onConnect.addListener(function(port) {
+	    console.log("Port name: " + port.name);
+	    port.onMessage.addListener(function(msg) {
+		if (msg.text == "Connecting") {
+		    console.log("Opening connection received");
+		    port.postMessage({text: "Listening"});
 		}
+		else if (msg.text == "Confirmed") {
+		    console.log("Connection confirmed, good to go!");
+		}
+		else
+		    console.log("Something went wrong, background");
+	    });
 	});
+
+	//Listener for when popup.js requests something
+	chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+		console.log("Sender " + sender);
+		console.log("Request " + request.detail);
+		getSpooks(activeTab, false);
+		console.log("Sent body " + spookCount);
+		sendResponse({detail: "active", active: activeTab, count: spookCount});
+	});
+
+	//Listener for when the active tab is switched
+	chrome.tabs.onActivated.addListener(function(active) {
+		activeTab = active.tabId;
+		console.log("Tab ID " + activeTab);
+		getSpooks(activeTab);
+
+	});
+
+	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+		getSpooks(tabId);
+	});
+
+
+	chrome.extension.onMessage.addListener( function(request, sender) {
+		if(request.action == "play") {
+			playSound();
+		}
+		else if (request.action == "stop") {
+			stopSound();
+		}
+		else if (request.action == "pause") {
+			pauseSound();
+		}
+		else
+			console.log("Unrecognized action");
+	});
+
 }
-
-//Listener for the connection call from the content script
-chrome.runtime.onConnect.addListener(function(port) {
-    console.log("Port name: " + port.name);
-    port.onMessage.addListener(function(msg) {
-        if (msg.text == "Connecting") {
-            console.log("Opening connection received");
-            port.postMessage({text: "Listening"});
-        }
-        else if (msg.text == "Confirmed") {
-            console.log("Connection confirmed, good to go!");
-        }
-        else
-            console.log("Something went wrong, background");
-    });
-});
-
-//Listener for when popup.js requests something
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-	getSpooks(activeTab);
-	console.log("Sender " + sender);
-	console.log("Request " + request.deatil);
-	console.log("Sent body " + spookCount);
-	sendResponse({detail: "active", active: activeTab, count: spookCount});
-});
-
-//Listener for when the active tab is switched
-chrome.tabs.onActivated.addListener(function(active) {
-	activeTab = active.tabId;
-	console.log("Tab ID " + activeTab);
-	getSpooks(activeTab);
-});
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	getSpooks(tabId);
-});
--- popup.js
+++ popup.js
@@ -1,56 +1,36 @@
 window.onload = function() {
-	try{
-		var allElements = document.getElementsByTagName("*");
-		var spookCounter = 0;
-		var sound = document.getElementById("spook_sound");
-		var alltext = -1;
-	}
-	catch(err) {
-		console.log("issue with declaring");
-	}
-
-	sound.load();
-
-
-	//function play_audio(set) {
-	//    if(set == 'play'){
-	//	    if(sound.paused){
-	//		    sound.play();
-	//	    }
-	//    }
-	//    if(set == 'stop'){
-	//	sound.pause()
-	//	sound.currentTime = 0;
-	//    }
-	//}
-	function playSound() {
-		setTimeout(function() {
-			sound.play();
-		}, 150);
-	}
-
-	function stopSound() {
-		setTimeout(function() {
-			sound.pause();
-			sound.currentTime = 0;
-		}, 150);
-	}
-
-	//compliance with chrome's extension CSP
-	document.getElementById("play").addEventListener('click', playSound);
-	document.getElementById("stop").addEventListener('click', stopSound);
-
+	
 	chrome.extension.sendRequest({detail: "info"}, function handler(response) {
 	    if (response == null) {
 		console.log("Empty response");
 		document.getElementById("spooks").innerHTML = "Empty response";
 	    }
 	    else if (response.detail == "active") {
-		document.getElementById("spooks").innerHTML = response.count;
+		spookCount = response.count;
+		document.getElementById("spooks").innerHTML = spookCount;
+		if(spookCount == 0) {
+			document.getElementById("spooky_stuff").style.display ='none';
+			//document.getElementById("title").style.paddingTop = '75px';
+		}
+		else {
+			document.getElementById("spooky_stuff").style.visibility='block';
+			//document.getElementById("title").style.paddingTop = '25px';
+		}
 		console.log("good response");
-		sound.play();
 	    }
 	    else
 		console.log("something went wrong in request response");
+	});
+
+	document.getElementById("play").addEventListener('click', function() {
+		chrome.extension.sendMessage({action: "play"});
+	});
+
+	document.getElementById("stop").addEventListener('click', function() {
+		chrome.extension.sendMessage({action: "stop"});
+	});
+
+	document.getElementById("pause").addEventListener('click', function() {
+		chrome.extension.sendMessage({action: "pause"});
 	});
 }
